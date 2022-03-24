31d23aaae34e7e64db07136bb26f7093493a1b99
vikrambhagat16 <102277591+vikrambhagat16@users.noreply.github.com>
2022-03-24 19:14:35 +0530

    Assignment_3_TransactionTest
    Assignment_3_TransactionTest

---
+++ TransactionDetails/Transaction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Humanizer;
+
+namespace TransactionDetails
+{
+    public class Transaction
+    {
+        public decimal Amount { get; }
+        public string AmountForHuman
+        {
+            get { return ((int)Amount).ToWords(); }
+        }
+        public DateTime Date { get; }
+        public string Notes { get; }
+        public Transaction(decimal amount, DateTime date, string notes)
+        {
+            Amount = amount;
+            Date = date;
+            Notes = notes;
+        }
+
+        public void printAccountDetails()
+        {
+            Console.WriteLine("Amount : "+Amount + " Date : "+ Convert.ToDateTime(Date).ToString("dd/MM/yyyy"));
+            Console.WriteLine("Human Readable Amount : " + AmountForHuman);
+        }
+    }
+
+}
